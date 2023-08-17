223a347524416964da068a461bbb58869b76600d
Juanchi <toranzos628@gmail.com>
2023-08-17 17:48:43 -0300

    un ejercicio de condicionales

--- js/2-condicionalesEJ2.js
+++ js/2-condicionalesEJ2.js
@@ -1,6 +1,17 @@
-//pedirle al usuario que seleccione un mes y le informamos si el mes seleccionado tiene entre 31/ 30 dias o 29 y 29
-//31 dias- enero, marzo, mayo, julio, agosto, octubre, diciembre
-//30 dias- abril, junio, sep, nov
-//28 o 29 dias- febrero
- let mes = parseInt( promt('ingrese el numero del mes(1 al 12)'));
- if()
+//16 a 18 años es optativo
+// 18 hasta 70 es obligatorio
+// mayores de 70 es optativo
+// quiero saber si es la primera vez que vota el usuario
+let edadUsuario = parseInt(prompt('Ingrese su edad'));
+if(edadUsuario >= 16 && edadUsuario < 18 || edadUsuario >=71){
+    document.write('es optativo que votes')
+}else{
+   if(edadUsuario >=18 && edadUsuario <= 70){
+    document.write('es obligatorio votar')
+   }else{
+    document.write('no podes votar')
+
+   }
+}
+17
+//preguntar si es >= a 16
