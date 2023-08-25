90de5aa87528d845c6730a350dab3766565a6e67
Juanchi <toranzos628@gmail.com>
2023-08-25 15:22:13 -0300

    fin ejercicio 10 de bucles

--- js/ejerciciostp/ejercicio10-TP2.js
+++ js/ejerciciostp/ejercicio10-TP2.js
@@ -1,17 +1,17 @@
 // 10- Realiza un script que pida número de filas y columnas y escriba una tabla. Dentro de cada una de las celdas deberá escribirse un número consecutivo en orden descendente. si,por ejemplo, la tabla es de 7x5 los números irán del 35 al 1.
+
 const filas = parseInt(prompt('ingrese un numero de filas'));
-const columnas =parseInt(prompt('ingrese un numero de columnas'));
+const columnas = parseInt(prompt('ingrese un numero de columnas'));
 let numeroCeldas = filas * columnas
 document.write(`  <table><tbody>`)
-for(let indiceFilas = 0 ; indiceFilas < filas ; indiceFilas++){
+for (let indiceFilas = 0; indiceFilas < filas; indiceFilas++) {
     document.write('<tr>')
-    for(indiceColumna = 0; indiceColumna < columnas ; indiceColumna++){
-        document.write('<td>1</td>')
+    for (indiceColumna = 0; indiceColumna < columnas; indiceColumna++) {
+        document.write(`<td>${numeroCeldas}</td>`)
+        numeroCeldas--;
     }
-   
+
     document.write('</tr>')
-    
+
 }
-
-
 document.write(` </tbody></table>`)
